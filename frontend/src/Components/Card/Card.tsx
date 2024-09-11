b47f86e72fd56077e573ba90bab89c857f0bf476
Sean <42322818+seangrant118@users.noreply.github.com>
2024-09-11 13:51:31 -0400

    Create Card

---
+++ frontend/src/Components/Card/Card.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+type Props = {}
+
+const Card = (props: Props) => {
+    return (
+        <div className='card'>
+            <img 
+                src="https://images.unsplash.com/photo-1612428978260-2b9c7df20150?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80"
+                alt="Image"
+            />
+            <div className="details">
+                <h2>AAPL</h2>
+                <p>$110</p>
+            </div>
+            <p className="info">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse neque nemo cumque voluptates unde accusamus nulla ad quod. Non minus cumque laudantium neque provident ratione natus aliquam officia omnis laborum!</p>
+        </div>
+        
+    )
+}
+
+export default Card
