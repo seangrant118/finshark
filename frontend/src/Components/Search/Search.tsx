069c5ab10b1e1b1db896beb4ec8535d0192f0692
Sean <42322818+seangrant118@users.noreply.github.com>
2024-09-13 14:41:11 -0400

    Added: Button event handler

--- frontend/src/Components/Search/Search.tsx
+++ frontend/src/Components/Search/Search.tsx
@@ -1,17 +1,22 @@
-import React, {useState} from 'react';
+import React, {ChangeEvent, useState} from 'react';
 
 type Props = {}
 
 const Search : React.FC<Props> = (props: Props): JSX.Element => {
     const [search, setSearch] = useState<string>("");
 
-    const onClick = (e: any) => {
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
         setSearch(e.target.value);
+    }
+
+    const onClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
         console.log(e)
     }
+
     return (
         <div>
-            <input type="text" name="" id="" value={search} onChange={(e) => onClick(e)}/>
+            <input type="text" name="" id="" value={search} onChange={(e) => handleChange(e)}/>
+            <button onClick={(e) => onClick(e)} />
         </div>
     )
 }
