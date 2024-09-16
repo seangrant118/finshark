d71d778750768d779ceef795825d2201568ae87a
Sean <42322818+seangrant118@users.noreply.github.com>
2024-09-16 15:27:37 -0400

    Add: add to portfolio functionality

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,10 +4,12 @@ import CardList from './Components/CardList/CardList';
 import Search from './Components/Search/Search'
 import { CompanySearch } from './company';
 import { searchCompanies } from './api';
+import ListPortfolio from './Components/Portfolio/ListPortfolio/ListPortfolio';
 
 
 function App() {
   const [search, setSearch] = useState<string>("");
+  const [portfolioValues, setPortfolioValues] = useState<string[]>([])
   const [searchResult, setSearchResult] = useState<CompanySearch[]>([])
   const [serverError, setServerError] = useState<string>("")
 
@@ -15,9 +17,12 @@ function App() {
       setSearch(e.target.value);
   }
 
-  const onPortfolioCreate = (e: SyntheticEvent) => {
+  const onPortfolioCreate = (e: any) => {
     e.preventDefault();
-    console.log(e)
+    const exists = portfolioValues.find((value) => value === e.target[0].value)
+    if (exists) return;
+    const updatedPortfolio = [...portfolioValues, e.target[0].value];
+    setPortfolioValues(updatedPortfolio);
   }
 
   const onSearchSubmit = async (e: SyntheticEvent) => {
@@ -34,6 +39,7 @@ function App() {
   return (
     <div className="App">
       <Search onSearchSubmit={onSearchSubmit} search={search} handleSearchChange={handleSearchChange} />
+      <ListPortfolio portfolioValues={portfolioValues}/>
       {serverError && <h1>{serverError}</h1>}
       <CardList searchResults={searchResult} onPortfolioCreate={onPortfolioCreate}/>
     </div>
