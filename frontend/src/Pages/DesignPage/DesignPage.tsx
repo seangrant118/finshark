824cb0bf78fa6a0644a300b0dc208ea858fc7404
Sean <42322818+seangrant118@users.noreply.github.com>
2024-09-20 11:19:04 -0400

    Add: Design Page

---
+++ frontend/src/Pages/DesignPage/DesignPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface Props {}
+
+const DesignPage = (props: Props) => {
+  return <>
+    <h1>FinShark Design Page</h1>
+    <h2>This is FinSharks design page. THis is where we will house various design aspects of the app</h2>
+    {/* <Table /> */}
+  </>
+};
+
+export default DesignPage;
