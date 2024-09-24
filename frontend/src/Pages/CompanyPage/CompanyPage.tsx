33036d08bb061f0495ba5b53ce44b438bf3c8518
Sean <42322818+seangrant118@users.noreply.github.com>
2024-09-24 13:24:50 -0400

    Add: 10k finder component

--- frontend/src/Pages/CompanyPage/CompanyPage.tsx
+++ frontend/src/Pages/CompanyPage/CompanyPage.tsx
@@ -6,6 +6,7 @@ import Sidebar from "../../Components/Sidebar/Sidebar";
 import Dashboard from "../../Components/Dashboard/Dashboard";
 import Tile from "../../Components/Tile/Tile";
 import Spinner from "../../Components/Spinner/Spinner";
+import TenKFinder from "../../Components/TenKFinder/TenKFinder";
 
 interface Props {}
 
@@ -30,6 +31,7 @@ const CompanyPage = (props: Props) => {
             <Tile title="Price" subtitle={company.price.toString()} />
             <Tile title="Sector" subtitle={company.sector} />
             <Tile title="DCF" subtitle={company.dcf.toString()} />
+            <TenKFinder ticker={company.symbol} />
             <p className="bg-white shadow rounded text-medium text-gray-900 p-3 mt-1 m-4">
               {company.description}
             </p>
--- frontend/src/api.tsx
+++ frontend/src/api.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { CompanyBalanceSheet, CompanyCashFlow, CompanyIncomeStatement, CompanyKeyMetrics, CompanyProfile, CompanySearch } from "./company";
+import { CompanyBalanceSheet, CompanyCashFlow, CompanyIncomeStatement, CompanyKeyMetrics, CompanyProfile, CompanySearch, CompanyTenK } from "./company";
 
 interface SearchResponse {
   data: CompanySearch[];
@@ -70,6 +70,17 @@ export const getCashFlow = async (query: string) => {
   try {
     const data = await axios.get<CompanyCashFlow[]>(
       `https://financialmodelingprep.com/api/v3/cash-flow-statement/${query}?limit=40&apikey=${process.env.REACT_APP_API_KEY}`
+    );
+    return data;
+  } catch (error: any) {
+    console.log("error message from api: ", error.message);
+  }
+};
+
+export const getTenK = async (query: string) => {
+  try {
+    const data = await axios.get<CompanyTenK[]>(
+      `https://financialmodelingprep.com/api/v3/sec_filings/${query}?type=10-K&page=0&apikey=${process.env.REACT_APP_API_KEY}`
     );
     return data;
   } catch (error: any) {
