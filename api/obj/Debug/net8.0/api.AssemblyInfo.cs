a089d246c7eb5eac4e78554fd2a5c0469d074713
Sean <seangrant118@gmail.com>
2024-09-25 15:33:13 -0400

    Add: StockDto

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87f9689881f085fa6221546b39e9ced1a128917d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b066820fbbabff7996d25eb2a184059763f76c3d")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
