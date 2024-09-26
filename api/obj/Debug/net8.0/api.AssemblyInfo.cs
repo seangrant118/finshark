de0ffa7ffa755d11ce0e85bc647cc0c5d283ce00
Sean <seangrant118@gmail.com>
2024-09-26 12:46:47 -0400

    Refactor: Add async await to stock controller

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8504e68a0f246af11cd1e626723c1fb300ac77b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fb4efa42040bbf814d8d86ffca206d42862c0b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
