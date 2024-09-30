eeabbb8f9b8d8760793f538c8ee6b32bc774c5f8
Sean <seangrant118@gmail.com>
2024-09-30 09:25:01 -0400

    feat: add data annotations to dtos

--- api/DTOs/Stock/CreateStockRequestDto.cs
+++ api/DTOs/Stock/CreateStockRequestDto.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,11 +8,21 @@ namespace api.DTOs.Stock
 {
     public class CreateStockRequestDto
     {
+        [Required]
+        [MaxLength(10, ErrorMessage = "Symbol Cannot be over 10 characters")]
         public string Symbol {get; set;} = string.Empty;
+        [Required]
         public string CompanyName {get; set;} = string.Empty;
+        [Required]
+        [Range(1,1000000000)]
         public decimal Purchase {get; set;}
+        [Required]
+        [Range(0.0001,100)]
         public decimal LastDiv {get; set;}
+        [Required]
+        [MaxLength(20, ErrorMessage = "Industry cannot be over 20 characters")]
         public string Industry {get; set;} = string.Empty;
+        [Range(1,10000000000)]
         public long MarketCap {get; set;}
     }
 }
