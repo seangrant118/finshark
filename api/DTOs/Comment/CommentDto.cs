73ca6102f18bda3eda278632070d6eca9c2596cf
Sean <seangrant118@gmail.com>
2024-10-02 09:58:16 -0400

    feat: associated users with their comments

--- api/DTOs/Comment/CommentDto.cs
+++ api/DTOs/Comment/CommentDto.cs
@@ -12,5 +12,6 @@ namespace api.DTOs.Comment
         public string Title {get; set;} = string.Empty;
         public string Content {get; set;} = string.Empty;
         public DateTime CreatedOn {get; set;} = DateTime.Now;
+        public string CreatedBy {get; set;} = string.Empty;
     }
 }
--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b7184165132731a14db0c71dfc8fdcb3a64f3dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee624697c5212972075c5ad483d5af983691108e")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
