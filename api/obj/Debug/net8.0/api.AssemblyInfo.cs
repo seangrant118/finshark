0191c92c835ff6a145e5bfaa6a81a9fe81f9c3d7
Sean <seangrant118@gmail.com>
2024-10-02 09:02:37 -0400

    feat: add portfolio post request

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4c6e1b96b333afe4d6465ec1efc844c857153cb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b69565fd27167a6663022a70b1957329ac08bcd0")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
