7b7184165132731a14db0c71dfc8fdcb3a64f3dc
Sean <seangrant118@gmail.com>
2024-10-02 09:18:19 -0400

    feat: delete portfolio request

--- api/Controllers/PortfolioController.cs
+++ api/Controllers/PortfolioController.cs
@@ -73,5 +73,28 @@ namespace api.Controllers
                 return Created();
             }
         }
+
+        [HttpDelete]
+        [Authorize]
+        public async Task<IActionResult> DeletePortfolio(string symbol)
+        {
+            var username = User.GetUsername();
+            var appUser = await _userManager.FindByNameAsync(username);
+
+            var userPortfolio = await _portfolioRepo.GetUserPortfolio(appUser);
+
+            var filteredStock = userPortfolio.Where(s => s.Symbol.ToLower() == symbol.ToLower()).ToList();
+
+            if (filteredStock.Count() == 1)
+            {
+                await _portfolioRepo.DeletePortfolio(appUser, symbol);
+            }
+            else 
+            {
+                return BadRequest("Stock is not in portfolio");
+            }
+
+            return Ok();
+        }
     }
 }
--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b69565fd27167a6663022a70b1957329ac08bcd0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0191c92c835ff6a145e5bfaa6a81a9fe81f9c3d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
